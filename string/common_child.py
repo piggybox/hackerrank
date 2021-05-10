260b7e0d9a9f5e3b988a0f1015f0a1d1920331e3
Sky Yin <sky.yin@gmail.com>
2021-05-10 09:23:19 -0700

    solution to common child

---
+++ string/common_child.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# medium
+
+#
+# Complete the 'commonChild' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def commonChild(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    c = [[0 for i in range(m+1)] for j in range(n+1)]
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if s1[i-1] == s2[j-1]:
+                c[i][j] = c[i-1][j-1] + 1
+            else:
+                c[i][j] = max(c[i][j-1], c[i-1][j])
+
+    return c[m][n]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = commonChild(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
